55c5ff4b229b1902c8ecb85181bd2b9d48723d0e
Gowtham Kumar Vulluri <gowthamkumarvulluri@gmail.com>
2023-07-04 19:22:34 +0530

    working fine

---
+++ apis/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from .models import User, Profile, App
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.hashers import make_password
+
+
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('username', 'email', 'password')
+
+#         def create(self, validated_data):
+#             user = User.objects.create(
+#                 username=validated_data["password"], password=make_password(validated_data["password"]))
+#             user.save()
+#             # Token.objects.create(user=user)
+#             return user
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+
+class AdminAppSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = App
+        fields = ('id', 'name', 'points', 'complete')
+
+
+class UserAppSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = App
+        fields = '__all__'
+
+
+class CreateAppSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = App
+        fields = '__all__'
---
+++ apis/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from .views import index, login_view, profileapps, register, app, getapp, listapp, saveapp, profile, LogoutView
+
+urlpatterns = [
+    path('', index, name="index"),
+    path('register', register.as_view(), name='register'),
+    path('login', login_view.as_view(), name="login"),
+    path('adminapp', app.as_view(), name="app"),  # admin tasks creation
+    path('allapps', listapp.as_view(), name="applist"),  # admin task
+    path('getapp/<int:pk>', getapp.as_view(),
+         name="getapp"),  # retrive app based on id
+    path('saveapp', saveapp.as_view(), name="saveapp"),  # save app
+    path('profile/<int:pk>', profile.as_view(), name="profile"),
+    path('logout',  LogoutView, name='logout'),
+    path('profapp/<int:id>', profileapps.as_view(), name="profileapps"),
+]
